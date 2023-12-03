6a517b6af7731e4934704c625c9ebfbdbda1ebad
Nolito10 <norlanreyes14rosales@gmail.com>
2023-12-02 19:41:13 -0600

    Agregar archivos de proyecto.

---
+++ FigurasGeometricas/IFiguraGeometrica.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigurasGeometricas
+{
+
+    // Definición de la clase abstracta FiguraGeometrica
+    public abstract class FiguraGeometrica
+    {
+        public abstract double CalcularArea();
+        public abstract double CalcularPerimetro();
+    }
+
+    // Definición de la interfaz IFiguraGeometrica
+    public interface IFiguraGeometrica
+    {
+        void MostrarInformacion();
+        void DibujarFigura();
+    }
+
+    // Clase Cuadrado
+    public class Cuadrado : FiguraGeometrica, IFiguraGeometrica
+    {
+        private double lado;
+
+        public Cuadrado(double lado)
+        {
+            this.lado = lado;
+        }
+
+        public override double CalcularArea()
+        {
+            return lado * lado;
+        }
+
+        public override double CalcularPerimetro()
+        {
+            return 4 * lado;
+        }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"Cuadrado - Lado: {lado}");
+        }
+
+        public void DibujarFigura()
+        {
+            Console.WriteLine("Dibujando un cuadrado...");
+        }
+    }
+
+    // Clase Rectángulo
+    public class Rectangulo : FiguraGeometrica, IFiguraGeometrica
+    {
+        private double baseRectangulo;
+        private double alturaRectangulo;
+
+        public Rectangulo(double baseRectangulo, double alturaRectangulo)
+        {
+            this.baseRectangulo = baseRectangulo;
+            this.alturaRectangulo = alturaRectangulo;
+        }
+
+        public override double CalcularArea()
+        {
+            return baseRectangulo * alturaRectangulo;
+        }
+
+        public override double CalcularPerimetro()
+        {
+            return 2 * (baseRectangulo + alturaRectangulo);
+        }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"Rectángulo - Base: {baseRectangulo}, Altura: {alturaRectangulo}");
+        }
+
+        public void DibujarFigura()
+        {
+            Console.WriteLine("Dibujando un rectángulo...");
+        }
+    }
+
+    // Clase Triángulo
+    public class Triangulo : FiguraGeometrica, IFiguraGeometrica
+    {
+        private double lado1;
+        private double lado2;
+        private double lado3;
+
+        public Triangulo(double lado1, double lado2, double lado3)
+        {
+            this.lado1 = lado1;
+            this.lado2 = lado2;
+            this.lado3 = lado3;
+        }
+
+        public override double CalcularArea()
+        {
+            // Fórmula de Herón para el área de un triángulo
+            double s = (lado1 + lado2 + lado3) / 2;
+            return Math.Sqrt(s * (s - lado1) * (s - lado2) * (s - lado3));
+        }
+
+        public override double CalcularPerimetro()
+        {
+            return lado1 + lado2 + lado3;
+        }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"Triángulo - Lado1: {lado1}, Lado2: {lado2}, Lado3: {lado3}");
+        }
+
+        public void DibujarFigura()
+        {
+            Console.WriteLine("Dibujando un triángulo...");
+        }
+    }
+
+    // Clase Círculo
+    public class Circulo : FiguraGeometrica, IFiguraGeometrica
+    {
+        private double radio;
+
+        public Circulo(double radio)
+        {
+            this.radio = radio;
+        }
+
+        public override double CalcularArea()
+        {
+            return Math.PI * radio * radio;
+        }
+
+        public override double CalcularPerimetro()
+        {
+            return 2 * Math.PI * radio;
+        }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"Círculo - Radio: {radio}");
+        }
+
+        public void DibujarFigura()
+        {
+            Console.WriteLine("Dibujando un círculo...");
+        }
+    }
+}
+
---
+++ FigurasGeometricas/Program.cs
@@ -0,0 +1,78 @@
+﻿using FigurasGeometricas;
+using System;
+using System.Collections.Generic;
+
+namespace FigurasGeometricas 
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<FiguraGeometrica> figuras = new List<FiguraGeometrica>();
+
+            while (true)
+            {
+
+                Console.WriteLine("1. Crear Cuadrado");
+                Console.WriteLine("2. Crear Rectángulo");
+                Console.WriteLine("3. Crear Triángulo");
+                Console.WriteLine("4. Crear Círculo");
+                Console.WriteLine("5. Mostrar Información");
+                Console.WriteLine("6. Salir");
+
+                Console.Write("Ingrese la opción: ");
+                int opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.Write("Ingrese el lado del cuadrado: ");
+                        double ladoCuadrado = Convert.ToDouble(Console.ReadLine());
+                        figuras.Add(new Cuadrado(ladoCuadrado));
+                        break;
+                    case 2:
+                        Console.Write("Ingrese la base del rectángulo: ");
+                        double baseRectangulo = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("Ingrese la altura del rectángulo: ");
+                        double alturaRectangulo = Convert.ToDouble(Console.ReadLine());
+                        figuras.Add(new Rectangulo(baseRectangulo, alturaRectangulo));
+                        break;
+                    case 3:
+                        Console.Write("Ingrese el lado 1 del triángulo: ");
+                        double lado1Triangulo = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("Ingrese el lado 2 del triángulo: ");
+                        double lado2Triangulo = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("Ingrese el lado 3 del triángulo: ");
+                        double lado3Triangulo = Convert.ToDouble(Console.ReadLine());
+                        figuras.Add(new Triangulo(lado1Triangulo, lado2Triangulo, lado3Triangulo));
+                        break;
+                    case 4:
+                        Console.Write("Ingrese el radio del círculo: ");
+                        double radioCirculo = Convert.ToDouble(Console.ReadLine());
+                        figuras.Add(new Circulo(radioCirculo));
+                        break;
+                    case 5:
+                        Console.WriteLine("Información de las figuras:");
+                        foreach (var figura in figuras)
+                        {
+                            if (figura is IFiguraGeometrica figuraGeometrica)
+                            {
+                                figuraGeometrica.MostrarInformacion();
+                                figuraGeometrica.DibujarFigura();
+                                Console.WriteLine($"Área: {figura.CalcularArea()}, Perímetro: {figura.CalcularPerimetro()}");
+                            }
+                        }
+                        break;
+                    case 6:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida. Por favor, ingrese una opción válida.");
+                        break;
+                }
+            }
+        }
+    }
+
+}
